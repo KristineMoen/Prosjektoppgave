c8c41c39aadf367f404582b0221eceab2a1b5203
KristineMoen <kjmoen@stud.ntnu.no>
2025-11-10 12:40:00 +0100

    gjort noen endringer

---
+++ Prisfølsomhet time - Aggregert.py
@@ -0,0 +1,65 @@
+from unittest import result
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import row
+
+import statsmodels.api as sm
+import patsy
+import mplcursors
+
+# -------------------------------------- LESER DATA --------------------------------------#
+
+data_demand = pd.read_csv('/Users/kristinemoen/Documents/5-klasse/Prosjektoppgave_CSV_filer/demand.csv')
+
+data_price = pd.read_csv('prices.csv')
+data_price_update = data_price.drop(columns = ['Price_NOK_MWh'])
+
+Blindern_Temp_t4t = pd.read_csv('Blindern_temperatur_t4t.csv')
+
+#------------------------------------- FINNE AKTUELLE HUSSTANDER -------------------------------------------#
+
+#Finne ID:
+data_answer = pd.read_csv('answers.csv')
+data_households = pd.read_csv('households (1).csv')
+liste_husstander = []
+
+def finne_husstander():
+    for index, rad in data_answer.iterrows():
+        if (
+                rad["Q_City"] == 4 and      # 4 = Oslo 5 = Bergen 6 = Tromsø 7 = Trondheim
+                rad["Q22"] == 1 and        # 1 = Enebolig 4 = Boligblokk
+                rad["Q23"] == 9 and        # 1= Under 30 kvm, 2 = 30-49 kvm, 3 = 50-59 kvm, 4 = 60-79 kvm, 5 = 80-99 kvm, 6 = 100-119 kvm, 7 = 120-159 kvm, 8 = 160-199 kvm, 9 = 200 kvm eller større, 10 = vet ikke
+                rad["Q21"] == 6         # 1 = Under 300 000 kr, 2 = 300 000 - 499 999, 3 = 500 000 -799 999, 4 = 800 000 - 999 999, 5 = 1 000 000 - 1 499 999, 6 = 1 500 000 eller mer, 7 = Vil ikke oppgi, 8 = Vet ikke
+                #rad["Q20"] == 4         # 1 = Ingen fullført utdanning, 2 = Grunnskole, 3 = Vgs, 4 = Høyskole/Uni lavere grad, 5 = Høyskol/Uni høyere grad
+                #rad["Q20"] == 5
+        ):
+
+            # Sjekk om ID finnes i data_households og har Demand_data = 'Yes'
+            id_verdi = rad["ID"]
+            match = data_households[
+                (data_households["ID"] == id_verdi) &
+                (data_households["Demand_data"] == "Yes")
+                ]
+            if not match.empty:
+                liste_husstander.append(int(id_verdi))
+
+    print("ID-er som oppfyller kravene:", liste_husstander)
+
+finne_husstander()
+
+#--------------------------------- REGNE PÅ PRISFØLSOMHET PER TIME FOR TIME ------------------------------------------#
+
+test_liste_husstander = [512] #Bare for test
+
+#-----------------------------------------------------------------------------------
+
+'''Regresjon for "direkte", ren regresjonsanalyse: demand = beta_0 + beta_1 *pris + beta_2 * Temperatur24 + beta_3 * Temepartur24^2 + Beta_4 * Temperatur24^3
+                                                             + Temperatur72 + Hour_i + Month + Hour_i * Temperatur72 + error'''
+
+def direkte_prisfølsomhet_time(test_liste_husstander, data_demand, data_price_update, data_households, Blindern_Temp_t4t):
+    start_dato = '2021-04-01'
+    end_dato ='2022-03-31'
+
