73e10cde15e8f7319af680484622e89bd7e1975f
KristineMoen <kjmoen@stud.ntnu.no>
2025-12-14 16:43:35 +0100

    gjort noen endringer

--- Price responsitivity - Oslo (K).py
+++ Price responsitivity - Oslo (K).py
@@ -29,11 +29,11 @@ liste_husstander = []
 def finne_husstander():
     for index, rad in data_answer.iterrows():
         if (
-                rad["Q_City"] in [4, 1, 2]     # 4 = Oslo, 2 = Lillestrøm, 1 = Bærum
-                #rad["Q23"] == 9         # 1= Under 30 kvm, 2 = 30-49 kvm, 3 = 50-59 kvm, 4 = 60-79 kvm, 5 = 80-99 kvm, 6 = 100-119 kvm, 7 = 120-159 kvm, 8 = 160-199 kvm, 9 = 200 kvm eller større, 10 = vet ikke
-                #rad["Q21"] == 6         # 1 = Under 300 000 kr, 2 = 300 000 - 499 999, 3 = 500 000 -799 999, 4 = 800 000 - 999 999, 5 = 1 000 000 - 1 499 999, 6 = 1 500 000 eller mer, 7 = Vil ikke oppgi, 8 = Vet ikke
-                #rad["Q29"] == 2         # 1 = Har elbil, 2 = Har ikke elbil
-                #rad["Q31"] == 1         # 1 = Styrer ikke ladning av elbil for å unngå timer med høye priser, 2 = Ja, manuelt, 3 = Ja, automatisk etter tidspunkt, 4 = Ja, automatisk etter timepris
+                rad["Q_City"] in [4, 1, 2] and    # 4 = Oslo, 2 = Lillestrøm, 1 = Bærum
+                #rad["Q23"] in [1,2,3]         # 1= Under 30 kvm, 2 = 30-49 kvm, 3 = 50-59 kvm, 4 = 60-79 kvm, 5 = 80-99 kvm, 6 = 100-119 kvm, 7 = 120-159 kvm, 8 = 160-199 kvm, 9 = 200 kvm eller større, 10 = vet ikke
+                rad["Q21"] in [5,6]          # 1 = Under 300 000 kr, 2 = 300 000 - 499 999, 3 = 500 000 -799 999, 4 = 800 000 - 999 999, 5 = 1 000 000 - 1 499 999, 6 = 1 500 000 eller mer, 7 = Vil ikke oppgi, 8 = Vet ikke
+                #rad["Q29"] == 1   and      # 1 = Har elbil, 2 = Har ikke elbil
+                #rad["Q31"] in [2,3,4]          # 1 = Styrer ikke ladning av elbil for å unngå timer med høye priser, 2 = Ja, manuelt, 3 = Ja, automatisk etter tidspunkt, 4 = Ja, automatisk etter timepris
         ):
 
             # Sjekk om ID finnes i data_households og har Demand_data = 'Yes'
@@ -120,7 +120,7 @@ def price_responsitivity(liste_husstander, data_demand, data_price_update, data_
 
 
     # --- Regresjons analyse ---
-    y, X = patsy.dmatrices('np.log(Demand_kWh) ~ C(Price_Group, Treatment(reference="Before_ref")) + Temperatur24 + '
+    y, X = patsy.dmatrices('Demand_kWh ~ C(Price_Group, Treatment(reference="Before_ref")) + Temperatur24 + '
                            'I(Temperatur24**2) + I(Temperatur24**3) + Temperatur72 + '
                            'C(Hour, Treatment(reference="1")) + C(Month, Treatment(reference="September"))',
                            data=df, return_type='dataframe', NA_action='drop')
@@ -131,7 +131,7 @@ def price_responsitivity(liste_husstander, data_demand, data_price_update, data_
     # ----------- PLOT AV RESULTATER ----------------- #
     # -------- Boksplott over priskategoriene --------- #
 
-    sns.set(style="whitegrid")
+    '''sns.set(style="whitegrid")
 
     plt.figure(figsize=(8, 6))
     sns.boxplot(x='Price_Group', y='Demand_kWh', data=df, hue='Price_Group', palette='Set2', legend=False)
@@ -228,7 +228,7 @@ def price_responsitivity(liste_husstander, data_demand, data_price_update, data_
     plt.title('Gjennomsnitt vs modellpredikert forbruk per time')
     plt.grid(True)
     plt.legend()
-    plt.show()
+    plt.show()'''
 
     # -------- Printe sammenlignbare versjoner av lineær ----------
     print('Low', model.params['C(Price_Group, Treatment(reference="Before_ref"))[T.Low]'] / len(liste_husstander))
